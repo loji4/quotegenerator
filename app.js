90eb0998e5772b2b17eb5fbe2733b865165a9700
loji4 <yogawithlindao@gmail.com>
2021-05-03 13:40:09 +1000

    added html and quotes to array

---
+++ app.js
@@ -0,0 +1,17 @@
+// Pseuodocode 
+let quotes = [
+   {quote:'Nothing will work unless you do',
+   author: 'Maya Angelou'},
+
+    {quote: 'Hakuna Matata',
+    author:'Timon and Pumba' },
+
+    {quote:'Next to trying and winning the the best thing is trying and failing',
+    author:'Lacy Maud Montgomery'}
+]
+
+i = 0 
+// store quotes in an array 
+// increment the quotes by one when the right arrow is clicked
+// decrement the quotes by one when the right arrow is clicked
+// change the inner html each time 
